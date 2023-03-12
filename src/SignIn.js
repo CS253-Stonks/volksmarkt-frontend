6bbeba92daeda3d42aaf010a33d87b64485d2879
udhav-varma <udhavv21@iitk.ac.in>
2023-03-12 16:05:50 +0530

    Trying to make login page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,31 +1,60 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SignIn = () => {
     const [userID, changeUserID] = useState('');
     const [Pass, changePass] = useState('');
-
+    const [loginStatus, setStatus] = useState('');
+    const [users, setUsers] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+    useEffect(() => {
+        fetch('http://localhost:8000/users').then(res => {
+            return res.json();
+        }).then((data) => {
+            setUsers(data);
+        })
+    }, [])
+    const tryLogin = (e) => {
+        var found = false;
+        console.log(userID);
+        console.log(Pass);
+        setStatus('');
+        setLoading(true);
+        e.preventDefault();
+        
+        users.map((user) => {
+            if(user['userID'] == userID && user['password'] == Pass){
+                setStatus("Login Successful. Redirecting...");
+                setLoading(false);
+                //redirect using useHistory: ToDo
+            }
+        })
+        if(!isLoading && loginStatus == ''){
+            console.log("there\n" + loginStatus + isLoading);
+            setStatus("Incorrect UserID or Password");
+        }
+    }
     return (  
-        <body className="SignIn">
-            <div className="SignIn">
-            <h1 className="SignInheader">
-                User Sign In
-            </h1>
-            <p>Do not have an account? <Link to='/SignUp'>Click here</Link></p>
-            <form >
-                <label>User ID</label>
-                <div className="input"></div>
-                <input type="text" required 
-                value={userID}
-                onChange={(e) => changeUserID(e.target.value)}/>
-                <label>Password</label>
-                <input type="Password" required 
-                value={Pass}
-                onChange={(e) => changePass(e.target.value)}/>
-                <button>Sign In</button>
-            </form>
-            </div>
-        </body>
+        <div className="SignIn">
+        <h1 className="SignInheader">
+            User Sign In
+        </h1>
+        <p>Do not have an account? <Link to='/SignUp'>Click here</Link></p>
+        <form onSubmit={tryLogin}>
+            <label>User ID</label>
+            <div className="input"></div>
+            <input type="text" required 
+            value={userID}
+            onChange={(e) => changeUserID(e.target.value)}/>
+            <label>Password</label>
+            <input type="Password" required 
+            value={Pass}
+            onChange={(e) => changePass(e.target.value)}/>
+            <button>Sign In</button>
+            <p>{loginStatus}</p>
+        </form>
+        <p></p>
+        </div>
         
     );
 }
