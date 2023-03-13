37c831cab806cbc7644ef3dc5efeb87ded1b4f4f
udhav-varma <udhavv21@iitk.ac.in>
2023-03-14 02:14:25 +0530

    Added mui theme

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,54 +1,110 @@
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const SignIn = () => {
-    const [userID, changeUserID] = useState('');
-    const [Pass, changePass] = useState('');
-    const [loginStatus, setStatus] = useState('');
-    const [users, setUsers] = useState(null);
-    const [isLoading, setLoading] = useState(false);
-    useEffect(() => {
-        fetch('http://localhost:8000/users').then(res => {
-            return res.json();
-        }).then((data) => {
-            setUsers(data);
-        })
-    }, [])
-    const tryLogin = (e) => {
-        e.preventDefault();
-        const match = users.filter((user) => {
-            return user['userID'] == userID && user['password'] == Pass;
-        })
-        if(match.length == 0){
-            setStatus("Incorrect User ID or Password");
-        }
-        else{
-            setStatus("Welcome back, " + match[0]['name']);
-        }
-    }
-    return (  
-        <div className="SignIn">
-        <h1 className="SignInheader">
-            User Sign In
-        </h1>
-        <p>Do not have an account? <Link to='/SignUp'>Click here</Link></p>
-        <form onSubmit={tryLogin}>
-            <label>User ID</label>
-            <div className="input"></div>
-            <input type="text" required 
-            value={userID}
-            onChange={(e) => changeUserID(e.target.value)}/>
-            <label>Password</label>
-            <input type="Password" required 
-            value={Pass}
-            onChange={(e) => changePass(e.target.value)}/>
-            <button>Sign In</button>
-            <p>{loginStatus}</p>
-        </form>
-        <p></p>
-        </div>
-        
-    );
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
- 
-export default SignIn;
+
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,73 +1,131 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-const Signup = () => {
-    const [userID, setUserID] = useState('');
-    const [name, setName] = useState('');
-    const [pass1, setPass1] = useState('');
-    const [pass2, setPass2] = useState('');
-    const [msg, setMsg] = useState('');
-    const [users, setUsers] = useState(null);
-    const tryRegister = (e) => {
-        e.preventDefault();
-        if(pass1 != pass2){
-            setMsg("The passwords don't match");
-        }
-        else{
-            fetch('http://localhost:8000/users').then(res => {
-                return res.json();
-            }).then(data => {
-                console.log(data);
-                setUsers(data);
-            });
-            const match = users.filter(user => {
-                return user['userID'] == userID;
-            })
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-            if(match.length != 0){
-                setMsg("Sorry, User ID already taken");
-            }
-            else{
-                const userDetails = {name, userID, pass1};
-                fetch('http://localhost:8000/users', {
-                    method: 'POST',
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify(userDetails)
-                } ).then(() => {
-                    console.log("new user added");
-                    setMsg("Registration successful");
-
-                });
-
-            }
-        }
-    }
-
-    return ( 
-        <div className="SignUp">
-            <h1>User Sign Up</h1>
-            <p>Already have an account? <Link to='/SignIn'>Sign In</Link></p>
-            <form onSubmit={tryRegister}>
-            <label>User ID</label>
-            <input type="text" required 
-            value={userID}
-            onChange={(e) => setUserID(e.target.value)}/>
-            <label>Name</label>
-            <input type="text" required 
-            value={name}
-            onChange={(e) => setName(e.target.value)}/>
-            <label>Enter Password</label>
-            <input type="password" required 
-            value={pass1}
-            onChange={(e) => setPass1(e.target.value)}/>
-            <label>Reenter Password</label>
-            <input type="password" required 
-            value={pass2}
-            onChange={(e) => setPass2(e.target.value)}/>
-            <button>Sign Up</button>
-            </form>
-            <p>{ msg }</p>
-        </div>
-    );
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
- 
-export default Signup;
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
