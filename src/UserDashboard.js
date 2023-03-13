cdcc80719ffb0baa92995fd500bd947558d0ca48
Devansh Jain <d.jain.182002@gmail.com>
2023-03-13 19:03:22 +0530

    Making UserDashboard

---
+++ src/UserDashboard.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+const getStoreData = () => {
+    return [
+        "Store1",
+        "Store2",
+        "Store3"
+    ]
+}
+
+const StoreIcon = () => {
+    
+}
